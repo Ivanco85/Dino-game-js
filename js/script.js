b6c9bd863dde79ef37db3dc959759cf3d07f66f4
Ivanco <ivan.wasilev21@gmail.com>
2022-12-13 23:24:25 +0300

    13.12.22

--- js/script.js
+++ js/script.js
@@ -1,46 +1,51 @@
 const dino = document.querySelector('#dino');
 const cactus = document.querySelector('#cactus');
 
-window.onkeydown = jumpDino;
+// window.onkeydown = jumpDino;
 
-console.log(window);
+// function jumpDino(event) {
 
+//     if (event.code === 'Space') {
+//         dino.classList.add('jump');
+//         setTimeout(function () {
+//             dino.classList.remove('jump');
+//         }, 1000)
+//     }
+// }
 
-function jumpDino(event) {
+// let stop = true
 
-    if (event.code === 'Space') {
+// function jampalert() {
+//     // console.log('cactus', cactus.offsetLeft);
+//     // console.log('dino', dino.offsetLeft);
+//     if (cactus.offsetLeft <= 292) {
+//         if (dino.offsetTop > 140) {
+//             if (stop === true) {
+//                 alert('Game over');
+//             }
+//         }
+//     }
+// }
+
+// setInterval(jampalert, 2);
+
+document.addEventListener('keydown', function (event) {
+    jump();
+});
+
+function jump() {
+    if (dino.classList != 'jump') {
         dino.classList.add('jump');
-        setTimeout(function () {
-            dino.classList.remove('jump');
-        }, 1000)
     }
-}
-// setTimeout(function () {
-//     console.log('cactus', cactus.offsetLeft);
-// }, 100)
-
-// setTimeout(function () {
-//     console.log('cactus', cactus.offsetLeft);
-// }, 200)
-
-// setTimeout(function () {
-//     console.log('cactus', cactus.offsetLeft);
-// }, 300)
-console.log(dino);
-
-let stop = true
-
-function jampalert() {
-    // console.log('cactus', cactus.offsetLeft);
-    // console.log('dino', dino.offsetLeft);
-    if (cactus.offsetLeft <= 292) {
-        if (dino.offsetTop > 140) {
-            if (stop === true) {
-                alert('Game over');
-            }
-        }
-    }
+    setTimeout(function () {
+        dino.classList.remove('jump')}, 300);
 }
 
-setInterval(jampalert, 2);
+let isAlive = setInterval ( function() {
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
 
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+        alert('Game over');
+    }
+}, 10);
